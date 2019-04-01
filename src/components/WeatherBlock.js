9ce11ca6467b5fe219846a59aeac69d2d09e7afc
DH <dh@DHs-MacBook-Pro.local>
2019-04-01 17:34:18 -0400

    Add weather emojis

--- src/components/WeatherBlock.js
+++ src/components/WeatherBlock.js
@@ -12,11 +12,25 @@ function Weatherblock(props) {
     }
 
     if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Heavy Cloud')) {
-        generalCondition = 'Cloudy';
+        generalCondition = 'Cloudy☁️';
     } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Light Cloud')) {
-        generalCondition = 'Partly Cloudy';
-    } else {
-        generalCondition = props.prevData.weather_state_name;
+        generalCondition = 'Partly Cloudy⛅️';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Snow')) {
+        generalCondition = 'Snow❄️';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Sleet')) {
+        generalCondition = 'Sleet';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Hail')) {
+        generalCondition = 'Hail';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Thunderstorm')) {
+        generalCondition = 'Thunderstorm⛈️';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Heavy Rain')) {
+        generalCondition = 'Heavy Rain🌧️';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Light Rain')) {
+        generalCondition = 'Light Rain🌦️';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Showers')) {
+        generalCondition = 'Showers☔';
+    } else if ((props.prevData.weather_state_name) && (props.prevData.weather_state_name === 'Clear')) {
+        generalCondition = 'Clear🌤️';
     }
 
     //<span className='weather-icon' style={{display: props.prevData.weather_state_abbr ? 'block' : 'none'}}><img alt='' src={`https://www.metaweather.com/static/img/weather/${props.prevData.weather_state_abbr}.svg`}/></span>
@@ -26,8 +40,8 @@ function Weatherblock(props) {
             <div style={{display: props.prevData.the_temp ? 'block' : 'none'}}>{theTempF} ºF / {theTempC} ºC</div>
             <div style={{display: props.prevData.weather_state_name ? 'block' : 'none'}}>{generalCondition}</div>
             <div style={{visibility: props.prevData.humidity ? 'visible' : 'hidden'}}>Humidity: {props.prevData.humidity}%</div>
-            <div className="wind-data"><div style={{display: props.prevData.wind_direction_compass ? 'block' : 'none'}}>Wind Direction: {props.prevData.wind_direction_compass}</div>
-            <div style={{display: props.prevData.wind_speed ? 'block' : 'none'}}>Wind Speed: {Math.round(props.prevData.wind_speed)}mph</div></div>
+            <div className="wind-data" style={{display: props.prevData.wind_direction_compass ? 'block' : 'none'}}>&nbsp;🍃Wind💨</div><div className="wind-data"><div style={{display: props.prevData.wind_direction_compass ? 'block' : 'none'}}>Direction: {props.prevData.wind_direction_compass}</div>
+            <div style={{display: props.prevData.wind_speed ? 'block' : 'none'}}>Speed: {Math.round(props.prevData.wind_speed)}mph</div></div>
         </div>
     )
 }

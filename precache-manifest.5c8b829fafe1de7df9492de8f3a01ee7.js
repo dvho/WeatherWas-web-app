c29be556d708c2952150222b3f2a9ce068619616
DH <dh@DHs-MacBook-Pro.local>
2019-04-01 16:25:52 -0400

    Adjust weather block css

--- precache-manifest.5c8b829fafe1de7df9492de8f3a01ee7.js
+++ precache-manifest.5c8b829fafe1de7df9492de8f3a01ee7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.d653cc00.js"
   },
   {
-    "revision": "a43e6ef4cb4dc8f84f59",
-    "url": "./static/js/main.fef1dad9.chunk.js"
+    "revision": "31c97603442200fac41f",
+    "url": "./static/js/main.04ba8c77.chunk.js"
   },
   {
     "revision": "5896cb9016c6e0e8f4aa",
     "url": "./static/js/2.28189cc0.chunk.js"
   },
   {
-    "revision": "a43e6ef4cb4dc8f84f59",
-    "url": "./static/css/main.8fb87a08.chunk.css"
+    "revision": "31c97603442200fac41f",
+    "url": "./static/css/main.16d131f2.chunk.css"
   },
   {
-    "revision": "2fdda9c6bd7d83cfbffe6e022c34001f",
+    "revision": "fea3c6a3ab34b6391664a219ea3340ab",
     "url": "./index.html"
   }
 ];

21d6e35b4105b42ebf62c2ca2992e3b1bcecbe9a
DH <dh@DHs-MacBook-Pro.local>
2019-04-03 12:59:33 -0400

    Add first page text

--- precache-manifest.ffe8d3adf0e75bf63af640230099cc5a.js
+++ precache-manifest.ffe8d3adf0e75bf63af640230099cc5a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.d653cc00.js"
   },
   {
-    "revision": "76807d1e00633e1e7695",
-    "url": "./static/js/main.2f30d443.chunk.js"
+    "revision": "ad6403f395642e3ad856",
+    "url": "./static/js/main.63699c6c.chunk.js"
   },
   {
     "revision": "5896cb9016c6e0e8f4aa",
     "url": "./static/js/2.28189cc0.chunk.js"
   },
   {
-    "revision": "76807d1e00633e1e7695",
-    "url": "./static/css/main.b9d9d5b1.chunk.css"
+    "revision": "ad6403f395642e3ad856",
+    "url": "./static/css/main.a943aaef.chunk.css"
   },
   {
-    "revision": "dbac6eeb4846a6422e367f4bfdf0f69d",
+    "revision": "d26509681561fbe41449c1a33ed242b8",
     "url": "./index.html"
   }
 ];

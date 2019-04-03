0ca4a62dce8f9a5002872b4a2006d9e1dd2f3666
DH <dh@DHs-MacBook-Pro.local>
2019-04-02 22:27:48 -0400

    Get rid of submit button

--- precache-manifest.795b8500c9af6eaaa61a75b95d2be166.js
+++ precache-manifest.795b8500c9af6eaaa61a75b95d2be166.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.d653cc00.js"
   },
   {
-    "revision": "7394ce361248c32fb612",
-    "url": "./static/js/main.ae269441.chunk.js"
+    "revision": "d3bd4cb0956da6f2ca54",
+    "url": "./static/js/main.f5c630bf.chunk.js"
   },
   {
     "revision": "5896cb9016c6e0e8f4aa",
     "url": "./static/js/2.28189cc0.chunk.js"
   },
   {
-    "revision": "7394ce361248c32fb612",
-    "url": "./static/css/main.a6dea8a8.chunk.css"
+    "revision": "d3bd4cb0956da6f2ca54",
+    "url": "./static/css/main.dd47acc9.chunk.css"
   },
   {
-    "revision": "eab3390bcc6a4b682adc28135880e8de",
+    "revision": "fda86e305a5d51d7584588177c21585a",
     "url": "./index.html"
   }
 ];

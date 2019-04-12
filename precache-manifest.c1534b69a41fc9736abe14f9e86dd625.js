e918c0b358b29397f3a8b1976ef3140236bf3b30
DH <dh@DHs-MacBook-Pro.local>
2019-04-12 05:55:32 -0400

    Remove extraneous step in cases where country choice has only one city with data or where state choice of United States has only one city with data

--- precache-manifest.c1534b69a41fc9736abe14f9e86dd625.js
+++ precache-manifest.c1534b69a41fc9736abe14f9e86dd625.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.d653cc00.js"
   },
   {
-    "revision": "b9ffc35acb8a2242b775",
-    "url": "./static/js/main.3ffd5a20.chunk.js"
+    "revision": "dacd57c5e4979861d9bc",
+    "url": "./static/js/main.7a2ae024.chunk.js"
   },
   {
     "revision": "5896cb9016c6e0e8f4aa",
     "url": "./static/js/2.28189cc0.chunk.js"
   },
   {
-    "revision": "b9ffc35acb8a2242b775",
+    "revision": "dacd57c5e4979861d9bc",
     "url": "./static/css/main.f9eb5ca8.chunk.css"
   },
   {
-    "revision": "cea0063ac59975eff2b837d394df8bd3",
+    "revision": "fd0ed9c7f0ab8d036f5a7fae5cf0f44b",
     "url": "./index.html"
   }
 ];
